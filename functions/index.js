821a89c2303999eb7f38c2c3ecfca4a9352cddfa
Abduh <sheikhabduh6@gmail.com>
2025-06-07 13:30:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,36 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+const {RtcTokenBuilder, RtcRole} = require("agora-access-token");
 
 // Import your new user actions
 const userActions = require("./userActions");
 
 // Export the callable function
 exports.deleteUserAccount = userActions.deleteUserAccount;
+
+const APP_ID = "530ba273ad0847019e4e48e70135e345";
+const APP_CERTIFICATE = "11b76d5a76324fe5b4c616db8e786333";
+
+exports.generateAgoraToken = onRequest((request, response) => {
+  const channelName = request.body.channelName;
+  const uid = request.body.uid;
+  const role = RtcRole.PUBLISHER;
+  const expirationTimeInSeconds = 3600; // Token expires in 1 hour
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+
+  const token = RtcTokenBuilder.buildTokenWithUid(
+      APP_ID,
+      APP_CERTIFICATE,
+      channelName,
+      uid,
+      role,
+      privilegeExpiredTs,
+  );
+  response.send({token});
+});
 
 
 // Create and deploy your first functions
